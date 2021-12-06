754810ebd2e6cc004070d7de1ab2b2908af9be5d
MecFeel <94075983+MecFeel@users.noreply.github.com>
2021-12-06 09:17:19 -0300

    06/12/2021 - 01

---
+++ index.js
@@ -0,0 +1,72 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+var platforms;
+
+function preload ()
+{
+    this.load.image('sky', 'Imports/assets/sky.png');
+    this.load.image('ground', 'Imports/assets/platform.png');
+    this.load.image('star', 'Imports/assets/star.png');
+    this.load.image('bomb', 'Imports/assets/bomb.png');
+    this.load.spritesheet('dude', 'Imports/assets/dude.png',
+    {frameWidth: 32, frameHeight: 48}
+    );
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+    });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+    });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+    });
+}
+
+function update ()
+{
+}

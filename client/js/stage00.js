bdeab31e5299e3a66c62dcc318988d860e277efd
MecFeel <94075983+MecFeel@users.noreply.github.com>
2022-02-14 11:42:44 -0300

    14/02/2022 - 02

--- client/js/stage00.js
+++ client/js/stage00.js
@@ -3,6 +3,10 @@
 var stage00 = new Phaser.Scene("Introdução");
 
 var map;
+var dica;
+var texto;
+var pagedica;
+var cursors;
 var tileset0;
 var terreno;
 var tileset1;
@@ -17,8 +21,7 @@ var touchY;
 var timedEvent;
 var timer = -1;
 var timerText;
-var life = 0;
-var lifeText;
+var life;
 var trilha;
 var jogador;
 var ice_servers = {
@@ -30,119 +33,205 @@ var midias;
 const audio = document.querySelector("audio");
 
 stage00.preload = function () {
-
   this.load.image("map", "./assets/fundo-pagegame-fundo01.png");
   this.load.image("pc", "./assets/object-pagegame-pc.png");
-  this.load.spritesheet("close", "./assets/button-pagegame-close.png", { frameWidth: 236, frameHeight: 43.1429});
-  this.load.spritesheet("dicas", "./assets/object-pagegame-dicas.png", { frameWidth: 202, frameHeight: 202}); 
+  this.load.spritesheet("close", "./assets/button-pagegame-close.png", {
+    frameWidth: 236,
+    frameHeight: 43.1429,
+  });
+  this.load.spritesheet("dicas", "./assets/object-pagegame-dicas.png", {
+    frameWidth: 202,
+    frameHeight: 202,
+  });
   this.load.image("entrada", "./assets/object-pagegame-entrada.png");
-  this.load.spritesheet("saida", "./assets/object-pagegame-saida.png", { frameWidth: 314, frameHeight: 93});
+  this.load.spritesheet("saida", "./assets/object-pagegame-saida.png", {
+    frameWidth: 314,
+    frameHeight: 93,
+  });
   this.load.image("status01", "./assets/player1-pagegame-status.png");
   this.load.image("status02", "./assets/player2-pagegame-status.png");
   this.load.image("senha", "./assets/senha-pagegame-inserir.png");
   this.load.image("confirmar", "./assets/subpage-pagegame-confirmar.png");
   this.load.image("subpage", "./assets/subpage-pagegame-fundo.png");
   this.load.image("dica01", "./assets/subpage-pagegame-fundo.png");
-  this.load.spritesheet("player1", "./assets/players1.png",
-    { frameWidth: 100, frameHeight: 150});
-  this.load.spritesheet("player2", "./assets/players2.png",
-    { frameWidth: 100, frameHeight: 150});
+  this.load.spritesheet("player1", "./assets/player1.png", {
+    frameWidth: 100,
+    frameHeight: 150,
+  });
+  this.load.spritesheet("player2", "./assets/player2.png", {
+    frameWidth: 100,
+    frameHeight: 150,
+  });
+  this.load.image("pagedica", "./assets/subpage-pagegame-fundo.png");
 };
 
 stage00.create = function () {
-  this.add.image(960, 540, 'map');
-  this.add.image(2880, 540, 'map');
-  this.add.image(2880, 540, 'pc');
-  this.add.image(1434, 771, 'dicas');
-  this.add.image(1673, 445, 'dicas');
-  this.add.image(486, 771, 'dicas');
-  this.add.image(247, 445, 'dicas');
-  this.add.image(960, 1033.5, 'entrada');
-  this.add.image(2880, 1033.5, 'entrada');
-  this.add.image(960, 46.5, 'saida');
-  this.add.image(2880, 46.5, 'saida', 2); 
-  this.add.image(214.5, 94, 'status01');
-  this.add.image(2134.5, 94, 'status02');
-  this.add.image(1802, 21.5715, 'close');
-  this.add.image(3721, 21.5715, 'close');
+  life = "";
 
-  /*
+  this.add.image(960, 540, "map");
+  this.add.image(2880, 540, "map");
+  player1 = this.physics.add.sprite(960, 771, "player1");
+  player2 = this.physics.add.sprite(2880, 771, "player2");
+  this.add.image(2880, 540, "pc");
+  this.add.image(960, 46.5, "saida");
+  this.add.image(2880, 46.5, "saida", 2);
+  this.add.image(960, 1033.5, "entrada");
+  this.add.image(2880, 1033.5, "entrada");
+  this.add.image(214.5, 94, "status01");
+  this.add.image(2134.5, 94, "status02");
+  this.add.image(1802, 21.5715, "close");
+  this.add.image(3721, 21.5715, "close");
+
+  dica = this.physics.add.staticGroup();
+
+  dica.create(1434, 771, "dicas");
+
+  pagedica = this.physics.add.sprite(960, 540, "pagedica");
+  pagedica.disableBody(false, true);
+
+  this.physics.add.collider(player1, dica, hitdica1, null, this);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("player1", {
+      start: 0,
+      end: 0,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("player1", {
+      start: 0,
+      end: 0,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "stopped",
+    frames: this.anims.generateFrameNumbers("player1", {
+      start: 0,
+      end: 0,
+    }),
+    frameRate: 5,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "up",
+    frames: this.anims.generateFrameNumbers("player1", {
+      start: 0,
+      end: 0,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "down",
+    frames: this.anims.generateFrameNumbers("player1", {
+      start: 0,
+      end: 0,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  // Frames para movimentaçao beatriz
   this.anims.create({
     key: "left1",
-    frames: this.anims.generateFrameNumbers("player1", {
-      start: 0,
-      end: 6,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  // Animação do jogador 2: a esquerda
-  this.anims.create({
-    key: "left2",
     frames: this.anims.generateFrameNumbers("player2", {
       start: 0,
-      end: 6,
+      end: 0,
     }),
     frameRate: 10,
     repeat: -1,
   });
 
-  // Animação do jogador 1: a direita
   this.anims.create({
     key: "right1",
-    frames: this.anims.generateFrameNumbers("player1", {
-      start: 15,
-      end: 21,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  // Animação do jogador 2: a direita
-  this.anims.create({
-    key: "right2",
     frames: this.anims.generateFrameNumbers("player2", {
-      start: 15,
-      end: 21,
+      start: 0,
+      end: 0,
     }),
     frameRate: 10,
     repeat: -1,
   });
 
-  // Animação do jogador 1: ficar parado (e virado para a direita)
   this.anims.create({
     key: "stopped1",
-    frames: this.anims.generateFrameNumbers("player1", {
-      start: 11,
-      end: 14,
-    }),
-    frameRate: 5,
-    repeat: -1,
-  });
-
-  // Animação do jogador 2: ficar parado (e virado para a direita)
-  this.anims.create({
-    key: "stopped2",
     frames: this.anims.generateFrameNumbers("player2", {
-      start: 11,
-      end: 14,
+      start: 0,
+      end: 0,
     }),
     frameRate: 5,
     repeat: -1,
   });
 
-  var socket = this.socket;
+  this.anims.create({
+    key: "up1",
+    frames: this.anims.generateFrameNumbers("player2", {
+      start: 0,
+      end: 0,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
 
-  this.socket.on("jogadores", function (jogadores) {
-    if (jogadores.primeiro === self.socket.id) {
-      // Define jogador como o primeiro
-      jogador = 1;
+  this.anims.create({
+    key: "down1",
+    frames: this.anims.generateFrameNumbers("player2", {
+      start: 0,
+      end: 0,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
 
-      // Personagens colidem com os limites da cena
-      player1.setCollideWorldBounds(true);
-    }});
-    */
+  cursors = this.input.keyboard.createCursorKeys();
+
+  texto = this.add.text(960, 540, life, {
+    fontSize: "32px",
+    fill: "#cccccc",
+  });
 };
- 
+
+stage00.update = function (time, delta) {
+  if (cursors.left.isDown) {
+    player1.body.setVelocityX(-500);
+  } else if (cursors.right.isDown) {
+    player1.body.setVelocityX(500);
+  } else {
+    player1.body.setVelocityX(0);
+  }
+  if (cursors.up.isDown) {
+    player1.body.setVelocityY(-500);
+  } else if (cursors.down.isDown) {
+    player1.body.setVelocityY(500);
+  } else {
+    player1.body.setVelocityY(0);
+  }
+  // Animação player1
+  if (cursors.left.isDown) {
+    player1.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player1.anims.play("right", true);
+  } else if (cursors.up.isDown) {
+    player1.anims.play("up", true);
+  } else if (cursors.down.isDown) {
+    player1.anims.play("down", true);
+  } else {
+    player1.anims.play("stopped", true);
+  }
+};
+
+function hitdica1(player1, dicas) {
+  life = "texto teste";
+  pagedica.enableBody(true, 960, 540, true, true);
+}
+
 export { stage00 };
